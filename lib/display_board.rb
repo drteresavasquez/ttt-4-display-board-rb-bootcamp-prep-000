7a7a54f1a52a0778215d5f68ced50fa73e98f64b
Teresa Terry <dr.terry2013@gmail.com>
2018-02-07 15:16:48 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,5 +5,10 @@ board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
 
 def display_board(arr)
   pipe = "|"
-  puts "#{arr[]}#{pipe}#{arr[]}#{pipe}#{arr[]}"
+  sep_line = "-"*11
+  puts "#{arr[0]}#{pipe}#{arr[1]}#{pipe}#{arr[2]}"
+  puts "#{sep_line}"
+  puts "#{arr[3]}#{pipe}#{arr[4]}#{pipe}#{arr[5]}"
+  puts "#{sep_line}"
+  puts "#{arr[6]}#{pipe}#{arr[7]}#{pipe}#{arr[8]}"
 end
