02314ec4688a0c075740e17e84c8ac18a1ea323b
Teresa Terry <dr.terry2013@gmail.com>
2018-02-07 15:18:50 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,7 +8,7 @@ def display_board(arr)
   sep_line = "-"*11
   puts " #{arr[0]} #{pipe} #{arr[1]} #{pipe} #{arr[2]} "
   puts "#{sep_line}"
-  puts "#{arr[3]}#{pipe}#{arr[4]}#{pipe}#{arr[5]}"
+  puts " #{arr[3]} #{pipe} #{arr[4]} #{pipe} #{arr[5]} "
   puts "#{sep_line}"
-  puts "#{arr[6]}#{pipe}#{arr[7]}#{pipe}#{arr[8]}"
+  puts " #{arr[6]} #{pipe} #{arr[7]} #{pipe} #{arr[8]} "
 end
